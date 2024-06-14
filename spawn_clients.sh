ca7f273228faceceef1b867332215636186b2237
Nikita <117029004+Som3-b0dy@users.noreply.github.com>
2024-06-14 09:23:51 +0300

    Update spawn_clients.sh

--- spawn_clients.sh
+++ spawn_clients.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 c=0
-while [ 1 ]; do
-    if (( $c < 50 )); then
-        echo "Spawning container $c"
-        $(docker run -it --rm --net="docker_default" docker-client) 2>/dev/null
-        c=$(($c+1))
-        sleep 3
-    fi
+while [ $c -lt 50 ]; do
+    echo "Spawning container $c"
+    $(docker run -it --rm --net="docker_default" docker-client) 2>/dev/null
+    c=$((c+1))
+    sleep 3
 done
